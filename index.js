55ab0f6ddebd576c2da877b243c30b619aa704f8
Bruno Oliveira <49489698+byoliveira@users.noreply.github.com>
2021-09-16 20:58:45 -0300

    Exercicio desenvolvido em aula, usando os métodos http (GET, POST, PUT)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const { v4 } = require("uuid")
+const { request, response } = require("express");
+const express = require("express"); // importei meu express
+const { v4 } = require("uuid"); 
 
-const app = express();
+const app = express(); // aqui estou atribuindo o express há uma variável
 
-app.use(express.json())
+app.use(express.json()) // aqui eu defino o tipo de dado a ser trafegado
+
 
 /**
  *  MÉTODOS HTTP:
@@ -13,12 +15,17 @@ app.use(express.json())
  *  DELETE - DELETAR
  */
 
-const usuarios = []
+
+const usuarios = [] // aqui fica armazenado todos os usuários
+
+app.get('/users', (request, response) => {
+    return response.json(usuarios) // retornar meu vetor de array
+})
 
 // http://localhost:3333/users
 app.post('/users', (request, response) => {
   //aqui eu recebo os dados do front-end
-  const { name, lastName, phone, sexo} = request.body;
+  const { name, lastName, phone, sexo} = request.body; 
 
   // aqui eu armazeno os dados recebidos em uma variável
   const usario = { id: v4(), name, lastName, phone, sexo};
@@ -27,6 +34,31 @@ app.post('/users', (request, response) => {
   usuarios.push(usario)
 
   return response.json(usario)
+})
+
+app.put('/users/:id', (request, response) => {
+  const { id } = request.params;   // aqui eu envio o id do usuário
+  const { name, lastName, phone, sexo } = request.body;
+
+
+  // aqui eu só valido se ele existe ou não dentro do meu vetor
+  const userIndex = usuarios.findIndex( usuario => usuario.id === id)
+
+  if( userIndex < 0 ) {
+    return response.status(400).json({ error: 'Usuário não encotrado'})
+  }
+
+  const usuario = {
+    id,
+    name, 
+    lastName, 
+    phone, 
+    sexo
+  } 
+
+  usuarios[userIndex] = usuario;
+
+  return response.json(usuario)
 })
 
 app.listen(3333)  //  http://localhost:3333
